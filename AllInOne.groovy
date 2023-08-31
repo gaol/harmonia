935daab462e2f8ce3a321b53a4f351c793d3c567
Lin Gao <aoingl@gmail.com>
2023-08-31 20:38:31 +0800

    initial commit on all-in-one

---
+++ AllInOne.groovy
@@ -0,0 +1,202 @@
+
+// This is a function can be run in Jenkins job dsl plugin, using `def allInOne = load "harmonia/AllInOne.groovy"`
+
+// not all core components are listed, just some common ones for convenient.
+CORE_COMPS = ["undertow", "elytron", "jboss-modules", "jboss-marshalling", "jboss-remoting", "xnio"]
+
+// a dictionary contains name and the version string of the components used in eap and wildfly-core
+COMP_VERSIONS = [
+        "wildfly-core": "version.org.wildfly.core",
+        "wildfly-core-eap": "version.org.wildfly.core",
+        "wildfly-core-private": "version.org.wildfly.core",
+        "undertow": "version.io.undertow",
+        "elytron": "version.org.wildfly.security.elytron",
+        "xnio": "version.org.jboss.xnio",
+        "jbos-remoting": "version.org.jboss.remoting",
+        "jboss-marshalling": "version.org.jboss.marshalling.jboss-marshalling",
+        "jboss-modules": "version.org.jboss.modules.jboss-modules",
+        "hibernate": "version.org.hibernate",
+        "infinispan": "version.org.infinispan",
+        "ejb-client": "version.org.jboss.ejb-client",
+        "hal": "version.org.jboss.hal.console",
+        "ironjacamar": "version.org.jboss.ironjacamar",
+        "narayana": "version.org.jboss.narayana",
+        "resteasy": "version.org.jboss.resteasy"
+]
+
+def nameOfGit(def giturl) {
+    def lastSlash = giturl.lastIndexOf('/')
+    return giturl.substring(lastSlash + 1)
+}
+
+def checkOutComp(def workdir, def comp, def core) {
+    echo "Check out component: $comp \n"
+    def giturl = comp['giturl']
+    def version = comp['version']
+    if (version == null && giturl == null) {
+        error "giturl must be specified for: $comp when version is not specified"
+    }
+    def compName = comp['name']
+    if (compName == null && giturl != null) {
+        compName = nameOfGit(giturl)
+    }
+    def versionName = comp.get("version.name", COMP_VERSIONS.get(compName))
+    if (versionName == null) {
+        def message = error "FAIL:: No version name found for component: ${compName}"
+        error "$message"
+    }
+    def versionFile = core ? "coreversions" : "versions"
+    sh: "mkdir -p $workdir/$compName"
+    def buildScripts = ""
+    if (version == null) {
+        echo "Check out $compName from $giturl ..."
+        dir("$workdir/$compName") {
+            git branch: comp['branch'], url: giturl
+        }
+        def branch = comp['branch']
+        def buildCmd = comp.get("build-command", "mvn clean install")
+        def jdk = comp['jdk']
+        def javaHomeSwitch = ""
+        if (jdk != null) {
+            if (jdk >= 17) {
+                javaHomeSwitch = "JAVA_HOME=\$JAVA17_HOME && "
+            } else if (jdk >= 11) {
+                javaHomeSwitch = "JAVA_HOME=\$JAVA11_HOME && "
+            }
+        }
+        def srcPathSwitch = ""
+        def path = comp['path']
+        if (path != null) {
+            srcPathSwitch = "/" + path
+        }
+        echo "source path: $srcPathSwitch"
+        def buildOpts = comp.get("build-options", "-DskipTests")
+        def wf_core_options = ""
+        if (compName == "wildfly-core" || compName == "wildfly-core-eap" || compName == "wildfly-core-private") {
+            wf_core_options = """
+if [ -f "$workspace/coreversions" ]; then
+    coreversions="\$(cat $workspace/coreversions)"
+fi
+"""
+        }
+        buildScripts = """#!/bin/bash
+set -ex
+echo "Build $compName, Branch to build and use is: $branch, the source path is: $srcPathSwitch"
+pushd $workdir/$compName$srcPathSwitch
+coreversions=""
+$wf_core_options
+$javaHomeSwitch $buildCmd $buildOpts \$coreversions \${MAVEN_SETTINGS_XML_OPTION}
+
+# get the version, and append it to versions file in workspace
+$javaHomeSwitch mvn \${MAVEN_SETTINGS_XML_OPTION} help:evaluate -Dexpression=project.version
+version="\$(mvn \${MAVEN_SETTINGS_XML_OPTION} help:evaluate -Dexpression=project.version | grep -e '^[^\\[]')"
+echo -n " -D${versionName}=\$version" >> $workspace/$versionFile
+popd
+        """
+    } else {
+        buildScripts = """#!/bin/bash
+set -ex
+echo -n " -D${versionName}=$version" >> $workspace/$versionFile
+        """
+    }
+
+    // return the full build scripts
+    return buildScripts
+}
+
+def prepareScripts () {
+    echo "Preparing scripts reading from payload.json in workspace: ${env.WORKSPACE}"
+    def payload = readJSON file: "${env.WORKSPACE}/payload.json", returnPojo: true
+    echo "\n=============\nTry to test the payload:\n$payload \n====================\n"
+    def workdir = "$workspace/workdir"
+
+    // components
+    def core_scripts_file = ""
+    def scripts_file = ""
+    if (payload.containsKey("components")) {
+        def comps = payload["components"]
+        for (comp in comps) {
+            def compName = comp['name']
+            if (comp.get("core", CORE_COMPS.contains(compName))) {
+                env.HAS_CORE_COMPONENTS = 'true'
+                def buildScripts = checkOutComp(workdir, comp, true)
+                def buildScriptFile = "$workdir/$compName/build-${compName}.sh"
+                writeFile file: "$buildScriptFile", text: buildScripts
+                core_scripts_file += "$buildScriptFile \n"
+            } else {
+                env.HAS_COMPONENTS = 'true'
+                def buildScripts = checkOutComp(workdir, comp, false)
+                def buildScriptFile = "$workdir/$compName/build-${compName}.sh"
+                writeFile file: "$buildScriptFile", text: buildScripts
+                scripts_file += "$buildScriptFile \n"
+            }
+        }
+        writeFile file: "$workspace/core_components", text: core_scripts_file
+        writeFile file: "$workspace/components", text: scripts_file
+    }
+
+    // wildfly-core
+    def wc = "wildfly-core"
+    if (payload.containsKey(wc)) {
+        env.HAS_CORE = 'true'
+        // generate scripts to build wildfly-core
+        def buildCommands = checkOutComp(workdir, payload[wc], false)
+        writeFile file: "$workdir/wildfly-core/build.sh", text: buildCommands
+    }
+
+    // eap
+    sh: 'mkdir -p $workdir/eap'
+    def eapGitUrl = "$env.GIT_REPOSITORY_URL"
+    def eapBranch = "$env.GIT_REPOSITORY_BRANCH"
+    def buildOptions = "$env.BUILD_OPTS"
+    def testOptions = "$env.TESTSUITE_OPTS"
+    if (payload.containsKey("eap")) {
+        def eap = payload['eap']
+        eapGitUrl = eap.get('giturl', eapGitUrl)
+        eapBranch = eap.get('branch', eapBranch)
+        buildCmd = eap.get("build-command", "mvn clean install")
+        buildOptions = eap.get('build-options', buildOptions)
+        testOptions = eap.get('test-options', testOptions)
+    }
+    dir("$workdir/eap") {
+        git branch: eapBranch, url: eapGitUrl
+    }
+    def buildCommands = """#!/bin/bash
+set -ex
+echo "Build eap"
+pushd $workdir/eap
+coreversions=""
+if [ -f "$workspace/coreversions" ]; then
+    coreversions="\$(cat $workspace/coreversions)"
+fi
+versions=""
+if [ -f "$workspace/versions" ]; then
+    versions="\$(cat $workspace/versions)"
+fi
+echo -e "Versions are: \$versions, Core versions are: \$coreversions"
+mvn clean install $buildOptions \$versions \$coreversions \${MAVEN_SETTINGS_XML_OPTION} -B
+popd
+        """
+    writeFile file: "$workdir/eap/build-eap.sh", text: buildCommands
+
+    def testCommands = """#!/bin/bash
+set -ex
+echo "Test EAP"
+pushd $workdir/eap/testsuite
+coreversions=""
+if [ -f "$workspace/coreversions" ]; then
+    coreversions="\$(cat $workspace/coreversions)"
+fi
+versions=""
+if [ -f "$workspace/versions" ]; then
+    versions="\$(cat $workspace/versions)"
+fi
+echo -e "Versions are: \$versions"
+mvn clean install $testOptions \$versions \$coreversions \$TESTSUITE_OPTS
+popd
+        """
+    writeFile file: "$workdir/eap/test-eap.sh", text: testCommands
+}
+
+// remember to return this to be able to run in pipeline job
+return this
---
+++ eap-7.4-all-in-one.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+#
+# Build EAP 7.4.x all in one
+#
+set -exo pipefail
+
+full_path="$(realpath "$0")"
+dir_path="$(dirname "$full_path")"
+source "${dir_path}/eap-job/base.sh"
+
+readonly LOCAL_REPO_DIR=${LOCAL_REPO_DIR:-${WORKSPACE}/maven-local-repository}
+readonly MEMORY_SETTINGS=${MEMORY_SETTINGS:-'-Xmx2048m -Xms1024m'}
+readonly SUREFIRE_MEMORY_SETTINGS=${SUREFIRE_MEMORY_SETTINGS:-'-Xmx1024m'}
+readonly MAVEN_IGNORE_TEST_FAILURE=${MAVEN_IGNORE_TEST_FAILURE:-'false'}
+export MAVEN_WAGON_HTTP_POOL=${WAGON_HTTP_POOL:-'false'}
+readonly MAVEN_WAGON_HTTP_MAX_PER_ROUTE=${MAVEN_WAGON_HTTP_MAX_PER_ROUTE:-'3'}
+readonly SUREFIRE_FORKED_PROCESS_TIMEOUT=${SUREFIRE_FORKED_PROCESS_TIMEOUT:-'90000'}
+readonly RERUN_FAILING_TESTS=${RERUN_FAILING_TESTS:-'0'}
+readonly OLD_RELEASES_FOLDER=${OLD_RELEASES_FOLDER:-/opt/old-as-releases}
+readonly FOLDER_DOES_NOT_EXIST_ERROR_CODE='3'
+
+export MAVEN_SETTINGS_XML=${MAVEN_SETTINGS_XML-'/opt/tools/settings.xml'}
+export MAVEN_VERBOSE=${MAVEN_VERBOSE}
+export FAIL_AT_THE_END=${FAIL_AT_THE_END:-'-fae'}
+export BUILD_OPTS=${BUILD_OPTS:-'-Drelease'}
+export MAVEN_HOME=${MAVEN_HOME:-'/opt/apache/maven'}
+export MAVEN_OPTS=${MAVEN_OPTS:-'-Dmaven.wagon.http.ssl.insecure=true -Dhttps.protocols=TLSv1.2'}
+export JAVA_HOME=${JAVA_HOME:-'/opt/oracle/java'}
+export JAVA11_HOME=${JAVA11_HOME:-'/opt/oracle/openjdk11-latest'}
+export JAVA17_HOME=${JAVA17_HOME:-'/opt/oracle/jdk17-latest'}
+
+# ${WORKSPACE} is under the workdir/ directory specified by build-wrapper.sh
+if [ -n "${WORKSPACE}" ]; then
+  echo -n "inside workspace: ${WORKSPACE}"
+fi
+echo '.'
+
+check_java
+configure_mvn_home
+configure_mvn_opts
+configure_mvn_settings
+
+# each time it runs one of the following:
+if [ "${BUILD_COMMAND}" = 'core-components' ]; then
+  # build the core components
+  core_scripts_file="${WORKSPACE}/../core_components"
+  echo "build the core components by executing components file in workspace directory"
+  if [ -f "$core_scripts_file" ]; then
+    # shellcheck disable=SC2013
+    for sf in $(cat "${core_scripts_file}"); do
+      echo "Executing script: $sf"
+      bash -ex "${sf}" 2>&1
+    done
+  fi
+elif [ "${BUILD_COMMAND}" = 'components' ]; then
+  # build components of eap
+  scripts_file="${WORKSPACE}/../components"
+  echo "build the components of EAP by executing components file in workspace directory"
+  if [ -f "$scripts_file" ]; then
+    # shellcheck disable=SC2013
+    for sf in $(cat "${scripts_file}"); do
+      echo "Executing script: $sf"
+      bash -ex "${sf}" 2>&1
+    done
+  fi
+elif [ "${BUILD_COMMAND}" = 'core' ]; then
+  # build core
+  echo "build core in ${WORKSPACE}/wildfly-core/"
+  bash -ex "${WORKSPACE}/wildfly-core/build.sh" 2>&1
+elif [ "${BUILD_COMMAND}" = 'eap-build' ]; then
+  # build eap
+  echo "build eap in ${WORKSPACE}/eap/"
+  # mvn clean install ${MAVEN_VERBOSE}  "${FAIL_AT_THE_END}" ${MAVEN_SETTINGS_XML_OPTION} -B ${BUILD_OPTS}
+  bash -ex "${WORKSPACE}/eap/build-eap.sh" 2>&1
+elif [ "${BUILD_COMMAND}" = 'eap-test' ]; then
+  # test eap
+  echo "Test eap in ${WORKSPACE}/eap/"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dsurefire.forked.process.timeout=${SUREFIRE_FORKED_PROCESS_TIMEOUT}"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dskip-download-sources -B"
+  #export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Djboss.test.mixed.domain.dir=${OLD_RELEASES_FOLDER}"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dmaven.test.failure.ignore=${MAVEN_IGNORE_TEST_FAILURE}"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dsurefire.rerunFailingTestsCount=${RERUN_FAILING_TESTS}"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dsurefire.memory.args=${SUREFIRE_MEMORY_SETTINGS}"
+  export TESTSUITE_OPTS="${TESTSUITE_OPTS} ${MAVEN_SETTINGS_XML_OPTION}"
+  # mvn clean install ${MAVEN_VERBOSE} "${FAIL_AT_THE_END}" ${TESTSUITE_OPTS}
+  bash -ex "${WORKSPACE}/eap/test-eap.sh" 2>&1
+fi
+
+
